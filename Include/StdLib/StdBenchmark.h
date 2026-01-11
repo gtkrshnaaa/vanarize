9d97ce41d01fc7f7fbcd4c4fed34745502a49f34
gtkrshnaaa <gtkrshnaaa@gmail.com>
2026-01-11 12:18:55 +0700

    Add StdBenchmark header

---
+++ Include/StdLib/StdBenchmark.h
@@ -0,0 +1,10 @@
+#ifndef VANARIZE_STDLIB_STDBENCHMARK_H
+#define VANARIZE_STDLIB_STDBENCHMARK_H
+
+#include "Core/VanarizeValue.h"
+#include <stdint.h>
+
+void StdBenchmark_Start(void);
+void StdBenchmark_End(Value iterations);
+
+#endif // VANARIZE_STDLIB_STDBENCHMARK_H
